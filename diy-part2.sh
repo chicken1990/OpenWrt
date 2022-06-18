206d97bd6a39efe533aa6dbc554f1929bd210ad9
hui <chicken.19900726@gmail.com>
2022-06-18 18:27:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,4 +14,4 @@
 sed -i 's/192.168.1.1/192.168.0.100/g' package/base-files/files/bin/config_generate
 
 # 5.修改默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
