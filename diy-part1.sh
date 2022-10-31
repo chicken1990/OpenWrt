5058853af2a2263ff41c8e923875418cde9dc85e
hui <chicken.19900726@gmail.com>
2022-10-31 14:59:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -27,11 +27,12 @@ git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-v
 
 #OpenClash项目
 
-#mkdir package/luci-app-openclash
-#cd package/luci-app-openclash
-#git init
-#git remote add -f origin https://github.com/vernesong/OpenClash.git
-#git config core.sparsecheckout true
-#echo "luci-app-openclash" >> .git/info/sparse-checkout
-#git pull origin master
-#git branch --set-upstream-to=origin/master master
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull origin master
+git branch --set-upstream-to=origin/master master
+cd ../..
