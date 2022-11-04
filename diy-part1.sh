86ae1e8a4f2003544bdd911a244f2832f21e9d08
hui <chicken.19900726@gmail.com>
2022-11-04 14:03:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,29 +21,4 @@ echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.co
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '$a src-git kiddin https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 
-#luci-app-vssr[Hello World]项目 
- 
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb  #git lua-maxminddb 依赖
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
 
-#OpenClash项目
-#库超容量，无法编译
-#mkdir package/luci-app-openclash
-#cd package/luci-app-openclash
-#git init
-#git remote add -f origin https://github.com/vernesong/OpenClash.git
-#git config core.sparsecheckout true
-#echo "luci-app-openclash" >> .git/info/sparse-checkout
-#git pull origin master
-#git branch --set-upstream-to=origin/master master
-#cd ../..
-
-## 下载OpenClash
-wget https://github.com/vernesong/OpenClash/archive/master.zip
-## 解压
-unzip master.zip
-## 复制OpenClash软件包到OpenWrt
-cp -r OpenClash-master/luci-app-openclash package
-
-
- 
